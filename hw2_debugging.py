9f2b1c62afb946b7b1a4ccd1e1e8c3f490a042c4
Poorna Subramanian <poorna.s.subramanian@gmail.com>
2024-09-09 00:17:37 +0000

    Fixed logical error in recombine function.

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -25,7 +25,7 @@ def merge_sort(arr):
     list: A new list containing all elements from the original array, sorted in ascending order.
 
     """
-    if len(arr) == 1:
+    if len(arr) <= 1:
         return arr
 
     half = len(arr) // 2
@@ -53,25 +53,30 @@ def recombine(left_arr, right_arr):
     """
     left_index = 0
     right_index = 0
+    current_index=0
     merge_arr = [None] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
-            merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
+            merge_arr[current_index] = left_arr[left_index]
             left_index += 1
-            merge_arr[left_index + right_index] = right_arr[right_index]
+        else:
+            merge_arr[current_index] = right_arr[right_index]
+            right_index += 1
+        current_index+=1
 
     for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+        merge_arr[current_index] = right_arr[i]
+        current_index+=1
 
     for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+        merge_arr[current_index] = left_arr[i]
+        current_index+=1
 
     return merge_arr
 
 
 arr_input = rand.random_array([None] * 20)
+print ("Input: ",arr_input)
 arr_out = merge_sort(arr_input)
 
-print(arr_out)
+print("Output: ",arr_out)
