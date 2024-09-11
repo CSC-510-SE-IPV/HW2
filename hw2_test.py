4f9dc62132b8d714a595be3f33a667f4ca0d2f49
Ishani Rajput <167826767+ishani-rajput@users.noreply.github.com>
2024-09-11 21:13:17 +0000

    Fixed test case file

--- hw2_test.py
+++ hw2_test.py
@@ -1,13 +1,13 @@
 import hw2_debugging
 
 def test_merge_sort1():
-    assert hw2_debugging.merge_sort([2,1,3]) == [1,2,3]
+    assert hw2_debugging.mergeSort([2,1,3]) == [1,2,3]
 
 def test_merge_sort2():
-    assert hw2_debugging.merge_sort([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert hw2_debugging.mergeSort([1, 2, 3, 4]) == [1, 2, 3, 4]
     
 def test_merge_sort3():
-    assert hw2_debugging.merge_sort([5, 1, 3, 5, 2]) == [1, 2, 3, 5, 5]
+    assert hw2_debugging.mergeSort([5, 1, 3, 5, 2]) == [1, 2, 3, 5, 5]
 
 def test_merge_sort4():
-    assert hw2_debugging.merge_sort([]) == []    
+    assert hw2_debugging.mergeSort([]) == []    
